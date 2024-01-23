68b4c67f6a374b446cd453716547f9d44271d306
Tariel <tariel@post.com>
2024-01-24 01:00:01 +0400

    Add controller CRUD methods

---
+++ Market/Controllers/ProductController.cs
@@ -0,0 +1,114 @@
+﻿using Market.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Market.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController: ControllerBase
+    {
+
+        [HttpGet("getProduct")]
+        public IActionResult GetProducts()
+        {
+            try
+            {
+                using (var context = new MarketContext())
+                {
+                    var products = context.Products.Select(x => new Product()
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        Description = x.Description
+                    });
+                    return Ok(products);
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPost("addProduct")]
+        public IActionResult AddProduct([FromQuery] string name, string description, int categoryId, int cost)
+        {
+            try
+            {
+                using (var context = new MarketContext())
+                {
+                    if (!context.Products.Any(x => x.Name.ToLower().Equals(name)))
+                    {
+                        context.Add(new Product()
+                        {
+                            Name = name,
+                            Description = description,
+                            Cost = cost,
+                            CategoryId = categoryId
+                        });
+                        context.SaveChanges();
+                        return Ok();
+                    }
+
+                    return StatusCode(409);
+
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpDelete("deleteProduct")]
+        public IActionResult DeleteProduct([FromQuery] int id)
+        {
+            try
+            {
+                using (var context = new MarketContext())
+                {
+                    if (!context.Products.Any(x => x.Id == id))
+                    {
+                        return NotFound();
+                    }
+
+                    Product product = context.Products.FirstOrDefault(x => x.Id == id)!;
+                    context.Products.Remove(product);
+                    context.SaveChanges();
+
+                    return Ok();
+
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPut("addProductPrice")]
+        public IActionResult AddProductPrice([FromQuery] int id, int cost)
+        {
+            try
+            {
+                using (var context = new MarketContext())
+                {
+                    if (!context.Products.Any(x => x.Id == id))
+                    {
+                        return NotFound();
+                    }
+
+                    Product product = context.Products.FirstOrDefault(x => x.Id == id)!;
+                    product.Cost = cost;
+                    context.SaveChanges();
+
+                    return Ok();
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
